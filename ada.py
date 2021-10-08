28328444038a8651e07f0101662728bc4755c27b
Sebastian Oliver <sebtheoliver@gmail.com>
2021-10-08 12:41:09 +0200

    Changed pin numbers.

--- ada.py
+++ ada.py
@@ -89,10 +89,10 @@ if(__name__=="__main__"):
     mcp = MCP.MCP3008(spi, cs)
 
     # create an analog input channel on pin 3 (light)
-    chan0 = AnalogIn(mcp, MCP.P3)
+    chan0 = AnalogIn(mcp, MCP.P0)
 
     # create an analog input channel on pin 2 (temp)
-    chan1 = AnalogIn(mcp, MCP.P2)
+    chan1 = AnalogIn(mcp, MCP.P1)
 
     #print("Raw ADC Value: ", chan.value)
     #print("ADC Voltage: " + str(chan.voltage) + "V")
